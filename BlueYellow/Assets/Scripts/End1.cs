897b47e18350406590b33d98631a3447dbe9b6c2
AtoosaHosseinikhah74 <36026281+AtoosaHosseinikhah74@users.noreply.github.com>
2018-08-04 01:26:17 +0430

    AddCodes

---
+++ BlueYellow/Assets/Scripts/End1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End1 : MonoBehaviour {
+	
+void OnTriggerEnter(Collider col)
+	{
+		 if (col.gameObject.tag == "Player")
+		 {
+			 print("End");
+			 //SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex+1);
+		 }
+	}
+}
