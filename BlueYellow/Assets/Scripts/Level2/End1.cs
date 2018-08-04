4ece367195fda97fb9fe8209a19da26e85733e84
atefehsaleh74 <36026321+atefehsaleh74@users.noreply.github.com>
2018-08-04 03:14:17 -0700

    EndGame

--- BlueYellow/Assets/Scripts/Level2/End1.cs
+++ BlueYellow/Assets/Scripts/Level2/End1.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class End1 : MonoBehaviour {
-	
+	public static int Count = 0;
 void OnTriggerEnter(Collider col)
 	{
-		 if (col.gameObject.tag == "Player")
+		 if (col.gameObject.tag == "P1")
 		 {
-			 print("End");
-			 //SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex+1);
+			 Count = 1;
+			 print("P1");
 		 }
 	}
 }
---
+++ BlueYellow/Assets/Scripts/Level2/End2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End2 : MonoBehaviour {
+	
+void OnTriggerEnter(Collider col)
+	{
+		 if (col.gameObject.tag == "P2")
+		 {
+			 End1.Count += 1;;
+			 print("P2");
+			 
+		 }
+	}
+
+	void Update()
+	{
+		if(End1.Count == 2)
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex+1);
+		}
+	}
+}
